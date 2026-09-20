ca96d263aa5948ba014e8b864d948a8394ceab2c
Hamada Ahmed <h_siana@yahoo.com>
2026-09-20 17:54:28 +0000

    point installer and updater to Script-modulat repository

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_URL="${HAMADA_REPO_URL:-https://github.com/Hamadaaahmed/commercial-vps-autoscript.git}"
+REPO_URL="${HAMADA_REPO_URL:-https://github.com/Hamadaaahmed/Script-modulat.git}"
 INSTALL_DIR="${HAMADA_DIR:-/root/commercial-vps-autoscript}"
 REF="${HAMADA_REF:-}"
 
